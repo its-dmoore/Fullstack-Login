250e28b3d2be51ba6d1abd19802a0a780eb4611f
its-dmoore <dtm36@njit.edu>
2023-04-10 23:49:34 -0400

    Mileston 1 complete

--- public_html/Project/profile.php
+++ public_html/Project/profile.php
@@ -117,7 +117,6 @@ $username = get_username();
         let con = form.confirmPassword.value;
         let isValid = true;
         //TODO add other client side validation....
-
         //example of using flash via javascript
         //find the flash container, create a new element, appendChild
         if (pw !== con) {
