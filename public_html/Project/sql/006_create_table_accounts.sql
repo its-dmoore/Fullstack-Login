1b46040e4f48e9dd9b53a5e17ed2ecd01582cb31
its-dmoore <dtm36@njit.edu>
2023-05-04 00:21:00 -0400

    More change

--- public_html/Project/sql/006_create_table_accounts.sql
+++ public_html/Project/sql/006_create_table_accounts.sql
@@ -9,7 +9,3 @@ CREATE TABLE IF NOT EXISTS RM_Accounts(
     FOREIGN KEY (user_id) REFERENCES Users(id),
     check ((account != '000000000000'balance >= 0) AND LENGTH(account) = 12)
 )
-INSERT INTO accounts (id, account_number, user_id, balance, account_type, created, modified) 
-SELECT -1, '000000000000', id, 0, 'world', NOW(), NOW() 
-FROM users 
-WHERE username = 'system';
